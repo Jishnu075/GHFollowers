33d4eddd4a993cba822e15af514d515d8d1cbd9e
Jishnu Biju <jishnu.biju@underarmour.com>
2024-01-18 13:35:33 +0530

    Initial commit

---
+++ GHFollowers/FollowersListVC.swift
@@ -0,0 +1,19 @@
+//
+//  FollowersListVC.swift
+//  GHFollowers
+//
+//  Created by jishnu biju on 18/01/24.
+//
+
+import UIKit
+
+class FollowersListVC: UIViewController {
+    
+    var username: String!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ GHFollowers/SearchVC.swift
@@ -0,0 +1,87 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by jishnu biju on 12/01/24.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+    
+    let logoImageView = UIImageView()
+    let usernameTextfield = GFTextfield()
+    let callToActionButton = GFButton(backgroundColor: .systemGreen, title: "Get Followers")
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureLogoImageView()
+        configureUsernameTextfield()
+        configureCallToActionButton()
+        createDismissTapGesture()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    func createDismissTapGesture(){
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func pushFollowersListVC() {
+        let followersListVC = FollowersListVC()
+        followersListVC.username = usernameTextfield.text
+        followersListVC.title = usernameTextfield.text
+        navigationController?.pushViewController(followersListVC, animated: true)
+    }
+    
+    func configureLogoImageView() {
+        view.addSubview(logoImageView)
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        logoImageView.image = UIImage(named: "gh-logo")!
+        
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.heightAnchor.constraint(equalToConstant: 200),
+            logoImageView.widthAnchor.constraint(equalToConstant: 200),
+        ])
+    }
+    
+    func configureUsernameTextfield() {
+        view.addSubview(usernameTextfield)
+        usernameTextfield.delegate = self
+        
+        NSLayoutConstraint.activate([
+            usernameTextfield.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 48),
+            usernameTextfield.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            usernameTextfield.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            usernameTextfield.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    func configureCallToActionButton() {
+        view.addSubview(callToActionButton)
+        callToActionButton.addTarget(self, action: #selector(pushFollowersListVC), for: .touchUpInside)
+        
+        NSLayoutConstraint.activate([
+            callToActionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            callToActionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            callToActionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            callToActionButton.heightAnchor.constraint(equalToConstant: 50),
+        ])
+        
+    }
+    
+}
+
+extension SearchVC: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        pushFollowersListVC()
+        return true
+    }
+}
